12d89072e8c65ec1fe9b906eb973c6080892dbd8
francis <francismugambi97@gmail.com>
2023-04-16 22:44:10 +0300

    First release of the website

---
+++ forms/contact.php
@@ -0,0 +1,32 @@
+<?php
+
+function strip_crlf($string)
+{
+    return str_replace("\r\n", "", $string);
+}
+
+if (! empty($_POST["send"])) {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $subject = $_POST["subject"];
+    $content = $_POST["message"];
+
+    $toEmail = "info@tecsol.co.ke";
+    // CRLF Injection attack protection
+    $name = strip_crlf($name);
+    $email = strip_crlf($email);
+    if (! filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "The email address is invalid.";
+    } else {
+        // appending \r\n at the end of mailheaders for end
+        $mailHeaders = "From: " . $name . "<" . $email . ">\r\n";
+        if (mail($toEmail, $subject, $content, $mailHeaders)) {
+            $message = "Your contact information is received successfully.";
+            $type = "success";
+            echo $message;
+            echo $type
+        }
+    }
+}
+require_once "contact.html";
+?>
